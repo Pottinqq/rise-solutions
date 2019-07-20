5ec95648bd89cb1106f832b5f4c5d64306b87532
Pottinqq <risehcf@dr.com>
2019-07-21 00:23:24 +0100

    kk

--- bot.js
+++ bot.js
@@ -1,23 +1,5 @@
 const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-});
+const bot = new Discord.Client();
 
 bot.on("ready", async () => {
   console.log(`Logged in as ${bot.user.tag} and is in ${bot.guilds.size} servers!`);
@@ -32,15 +14,50 @@ bot.on("message", async msg => {
   let bicon = bot.user.displayAvatarURL;
   let messageArray = msg.content.split(" ");
   let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,msg,args);
+  let args = messageArray.slice(1).join(' ');
 
   if (cmd.startsWith(prefix + "setgame")) {
-    var text = messageArray.slice(1).join(' ');
     if (msg.member.hasPermission("MANAGE_MESSAGES")) {
       msg.delete();
-      bot.user.setActivity(text);
+      bot.user.setActivity(args);
+    }
+    if (!msg.member.hasPermission("MANAGE_MESSAGES")) {
+      msg.delete();
+      let nopermembed = new Discord.RichEmbed()
+          .setTitle("No Permission")
+          .addField("You don't have permission", "you need the permission MANAGE_MESSAGES")
+          .setColor("#FF0000")
+          .setFooter("RiseSolutions#2662", bicon)
+          .setTimestamp();
+
+          msg.author.send(nopermembed);
+    }
+  }
+
+  if (cmd == `${prefix}help`) {
+   	let help = new Discord.RichEmbed()
+	.setTitle("Help")
+	.addField("**.setgame**", "Sets the bots game")
+	.setColor("#FF0000")
+	.setFooter("RiseSolutions | www.risesolutions.tk", bot.user.displayAvatarURL)
+	.setTimestamp();
+
+	msg.channel.send(`Messages are on the way to your dms :ticket:`);
+	msg.author.send(help);
+  }
+
+  if (cmd.startsWith(prefix + "s")) {
+  	const channel = bot.channels.get("name", "announcements");
+    if (msg.member.hasPermission("MANAGE_MESSAGES")) {
+    	let s = new Discord.RichEmbed()
+    	.setTitle(args)
+    	.setColor("#FF0000")
+    	.addField(args)
+    	.setFooter(`${msg.author.tag}`, bot.user.displayAvatarURL)
+    	.setTimestamp();
+      msg.delete();
+      channel.send("[ @here ]");
+      channel.send(s);
     }
     if (!msg.member.hasPermission("MANAGE_MESSAGES")) {
       msg.delete();
